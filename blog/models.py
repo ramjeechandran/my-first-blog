34d358df5c35ddafdf6ce7ea868b61dc125d1890
ramjee <ramjeechandran@gmail.com>
2018-09-22 15:06:54 +0530

    20180922-3.06

--- blog/models.py
+++ blog/models.py
@@ -5,6 +5,7 @@ class Post(models.Model):
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
+    #image = models.ImageField(upload_to='media', default="default.jpg")
     created_date = models.DateTimeField(
             default=timezone.now)
     published_date = models.DateTimeField(
